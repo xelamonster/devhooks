bef9599ceea140f042926b3a29503c700719da31
xelamonster <152045046+xelamonster@users.noreply.github.com>
2024-07-27 15:24:05 -0400

    type updates (#4)
    * 0.0.3
    
    * third time's the charm

--- lib/hooks.ts
+++ lib/hooks.ts
@@ -1,16 +1,35 @@
 import { debounce, type DebounceOptions } from "perfect-debounce"
-import { type DependencyList, useCallback, useEffect, useMemo, useReducer, useRef, useState } from "react"
-import { depsChanged, shallowMerge, type UnknownRecord } from "./utils"
+import {
+  type DependencyList,
+  type Dispatch,
+  type MutableRefObject,
+  useCallback,
+  useEffect,
+  useMemo,
+  useReducer,
+  useRef,
+  useState,
+} from "react"
+import {
+  type AsAsyncVoidFn,
+  type AsVoidFn,
+  depsChanged,
+  type InferredAsyncFn,
+  type InferredFn,
+  shallowMerge,
+  type UnknownRecord,
+  type UnPromise,
+} from "./utils"
 
 /**
  * `useAsyncCb` provides a sync callback for an async function
  * along with helpers to track the state.
  */
-export const useAsyncCb = <F extends (...args: Parameters<F>) => Promise<ReturnType<F>>>(
+export const useAsyncCb = <F extends InferredAsyncFn<F>>(
   fnAsync: F,
   onError?: (err: Error) => void,
-): [ReturnType<F> | undefined, Error | undefined, boolean, (...args: Parameters<F>) => void] => {
-  const [res, setRes] = useState<ReturnType<F> | undefined>(undefined)
+): [UnPromise<ReturnType<F>> | undefined, Error | undefined, boolean, AsVoidFn<F>] => {
+  const [res, setRes] = useState<UnPromise<ReturnType<F>> | undefined>(undefined)
   const [err, setErr] = useState<Error | undefined>(undefined)
   const [isReady, setIsReady] = useState(false)
   const fn = useCallback((...args: Parameters<F>) => {
@@ -27,7 +46,9 @@ export const useAsyncCb = <F extends (...args: Parameters<F>) => Promise<ReturnT
  * for most needs. The `state` is initialized to the given value, and the
  * `dispatch` function accepts fragments of `state` which are merged in.
  */
-export const useAutoReducer = <S extends UnknownRecord>(initState: S) =>
+export const useAutoReducer = <S extends UnknownRecord>(
+  initState: S,
+): [S, Dispatch<Partial<S> | ((prev: S) => Partial<S>)>] =>
   useReducer(
     (state: S, action: Partial<S> | ((prev: S) => Partial<S>)): S =>
       shallowMerge(state, typeof action === "function" ? action(state) : action),
@@ -38,8 +59,8 @@ export const useAutoReducer = <S extends UnknownRecord>(initState: S) =>
  * `useCurrentRef` escapes closures. `ref.current` will always be
  * the latest `val` even if `ref` is captured.
  */
-export const useCurrentRef = <T>(val: T) => {
-  const ref = useRef(val)
+export const useCurrentRef = <T>(val: T): MutableRefObject<T> => {
+  const ref = useRef<T>(val)
   ref.current = val
   return ref
 }
@@ -60,7 +81,7 @@ export const useDebounce = <T>(val: T, dur = 300): T => {
  * `useDebounceFn` consolidates multiple calls tp `fn` within duration `dur` into one.
  * Calls the given function immediately, use `opts` to change behavior.
  */
-export const useDebounceFn = <F extends (...args: Parameters<F>) => ReturnType<F>>(
+export const useDebounceFn = <F extends InferredFn<F>>(
   fn: F,
   dur = 300,
   opts: DebounceOptions = { leading: true },
@@ -76,15 +97,13 @@ export const useDebounceFn = <F extends (...args: Parameters<F>) => ReturnType<F
  * `useMutexFn` returns a callback that allows one call to `fn` and
  * ignores all subsequent calls until it completes.
  */
-export const useMutexFn = <F extends (...args: Parameters<F>) => Promise<ReturnType<F>>>(
-  fn: F,
-): (...args: Parameters<F>) => Promise<ReturnType<F> | undefined> => {
+export const useMutexFn = <F extends InferredAsyncFn<F>>(fn: F): AsAsyncVoidFn<F> => {
   const lockRef = useRef(false)
-  return useCallback(async (...args: Parameters<F>) => {
+  return useCallback(async (...args: Parameters<F>): Promise<void> => {
     if (lockRef.current) return
     lockRef.current = true
     try {
-      return await fn(...args)
+      await fn(...args)
     } finally {
       lockRef.current = false
     }
@@ -95,9 +114,9 @@ export const useMutexFn = <F extends (...args: Parameters<F>) => Promise<ReturnT
  * `useStaticMutexFn` is the same as `useMutexFn`, but the returned ref is wrapped
  * with `useStaticFn` instead of the standard `useCallback`.
  */
-export const useStaticMutexFn = <F extends (...args: Parameters<F>) => Promise<ReturnType<F>>>(fn: F) => {
+export const useStaticMutexFn = <F extends InferredAsyncFn<F>>(fn: F): AsAsyncVoidFn<F> => {
   const lockRef = useRef(false)
-  return useStaticFn(async (...args: Parameters<F>): Promise<ReturnType<F> | undefined> => {
+  return useStaticFn(async (...args: Parameters<F>): Promise<void> => {
     if (lockRef.current) return
     lockRef.current = true
     try {
@@ -126,7 +145,7 @@ export const useStaticMemo = <T>(gen: () => T, deps: DependencyList = []): T => 
  * `useStaticFn` gives a ref to `fn` which always calls the latest `fn`,
  * but won't trigger rerenders if `fn` changes.
  */
-export const useStaticFn = <F extends (...args: Parameters<F>) => ReturnType<F>>(fn: F): F => {
+export const useStaticFn = <F extends InferredFn<F>>(fn: F): F => {
   const ref = useRef<F>(fn)
   ref.current = useMemo<F>(() => fn, [fn])
   const staticRef = useRef<(...args: Parameters<F>) => ReturnType<F>>()
--- lib/utils.ts
+++ lib/utils.ts
@@ -25,3 +25,22 @@ export const isPromise = <T>(x: T | Promise<T>): x is Promise<T> => {
 }
 
 export type UnknownRecord = Record<string | number | symbol, unknown>
+// biome-ignore lint/suspicious/noExplicitAny: <explanation>
+export type UnknownFn = (...args: any) => any
+// biome-ignore lint/suspicious/noExplicitAny: <explanation>
+export type UnknownAsyncFn = (...args: any) => Promise<any>
+// biome-ignore lint/suspicious/noExplicitAny: <explanation>
+export type UnknownVoidFn = (...args: any) => void
+// biome-ignore lint/suspicious/noExplicitAny: <explanation>
+export type UnknownAsyncVoidFn = (...args: any) => Promise<void>
+export type InferredFn<F extends UnknownFn> = (...args: Parameters<F>) => ReturnType<F>
+export type InferredAsyncFn<F extends UnknownAsyncFn> = (
+  ...args: Parameters<F>
+  // biome-ignore lint/suspicious/noExplicitAny: <explanation>
+) => AsPromise<ReturnType<F>> & Promise<any>
+export type UnPromise<P> = P extends Promise<infer T> ? T : P
+export type AsPromise<P> = P extends Promise<infer T> ? Promise<T> : Promise<P>
+export type AsAsyncFn<F extends UnknownFn> = (...args: Parameters<F>) => AsPromise<ReturnType<F>>
+export type AsSyncFn<F extends UnknownFn> = (...args: Parameters<F>) => UnPromise<ReturnType<F>>
+export type AsVoidFn<F extends UnknownFn> = (...args: Parameters<F>) => void
+export type AsAsyncVoidFn<F extends UnknownFn> = (...args: Parameters<F>) => Promise<void>
